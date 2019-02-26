d56e7aebe0ae34bbe3cd932a9ed89197bf8d2626
TanteiTK <tkt6@hawaii.edu>
2019-02-26 10:00:18 -1000

    Initial Commit

---
+++ Assignment Six/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment Six
+//
+//  Created by CM Student on 2/26/19.
+//  Copyright © 2019 Tyler Kan-Tanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
